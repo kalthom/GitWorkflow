a5d134c2ed453ff0b8aa890cc3dadecd92816e18
kalthom <kalthum.mufath@gmail.com>
2025-04-11 19:13:39 +0300

    just an add

--- code.cpp
+++ code.cpp
@@ -10,7 +10,7 @@ using namespace std;
     {  
         string s;
         cin>>s;
-        cout << "Hello, " << s << endl;
+        cout << "Hello umm kalthum, " << s << endl;
        
       
         return 0;
